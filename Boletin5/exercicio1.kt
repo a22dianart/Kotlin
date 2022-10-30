50b349d4f4daa5e426d87fa15dc398f61079a411
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-30 16:05:15 +0100

    version1

---
+++ Boletin5/exercicio1.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val nums =arrayOf(7,10,3,34,13)
+    var min: Int = nums[0]
+    var max :Int = nums[0]
+    var parar = nums.size
+    while (parar>0){
+        if (nums[parar-1]>= max){
+            max=nums[parar-1]
+        }
+        if (nums[parar-1]<=min){
+            min=nums[parar-1]
+        }
+        parar =parar -1
+    }
+    println("O maximo e: $max")
+    print("O minimo e: $min")
+
+}
