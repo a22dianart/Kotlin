4c26ca18ee7442d1d8af58be249368f0ea33ad7d
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-30 15:55:21 +0100

    version1

---
+++ Boletin3/exercicio5.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var x =1
+    while (x<=3) {
+        println("1$x")
+        x=x+1
+    }
+    var y = 1
+    while (y<=3){
+        println("2$y")
+        y=y+1
+    }
+    var z = 1
+    while (z<=3){
+        println("3$z")
+        z=z+1
+    }
+}
