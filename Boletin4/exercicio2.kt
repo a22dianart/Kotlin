4300286852046a87e058c28e3df9a061547ce54e
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-30 16:01:14 +0100

    version1

---
+++ Boletin4/exercicio2.kt
@@ -0,0 +1,8 @@
+fun pensar (x:String){
+    println("Pienso que $x")
+}
+fun main(){
+    pensar("la vida es dura")
+    pensar("la cerveza se parece bastante al pis")
+    pensar("prefiero ser millonario que guapo")
+}
