b74dbb495a0c9adee848b1e4b44c7f6a1631f118
Diana <114477134+a22dianart@users.noreply.github.com>
2022-10-30 16:06:18 +0100

    version1

---
+++ Boletin5/exercicio4.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val notas =listOf(8.5,7.0,6.0,9.2)
+    var size = notas.size
+    var suma = 0.0
+    while (size>0){
+        suma= suma + notas[size-1]
+        size=size-1
+    }
+    var novosize =notas.size
+    print(suma/novosize)
+
+}
